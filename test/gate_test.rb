021958b800637101a1013a06cac4a5d70e5ce619
t.aoki11111 <t.aoki11111@gmail.com>
2019-07-01 01:35:16 +0900

    WIP

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,9 +1,35 @@
 require 'minitest/autorun'
 require './lib/gate'
+require './lib/ticket'
+
+# scenario test 1
+# 150円で切符を購入する
+# 梅田で入場、十三で出場
+# 期待結果:出場できる
 
 class GateTest < Minitest::Test
   def test_gate
-    # create Gate object
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
+  end
+end
+
+# scenario test 2
+# 150円で切符を購入する
+# 梅田で入場、三国で出場
+# 期待結果:出場できない
+
+class GateTest < Minitest::Test
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    refute mikuni.exit(ticket)
   end
 end
