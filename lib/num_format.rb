224bad0e767ee3028c1b0eb91eb7106f02465a35
t.aoki11111 <t.aoki11111@gmail.com>
2019-07-03 02:48:01 +0900

    Add 10進数を16進数に変換するスクリプト

---
+++ lib/num_format.rb
@@ -0,0 +1,28 @@
+# 10進数の11を16進数に変換(メソッドを使わない)
+num = 1967
+
+puts "最下位"
+one_num = num % 16
+puts one_num
+
+puts "あまり"
+num = num / 16
+puts num
+
+puts "二番目"
+two_num = num % 16
+puts two_num
+
+puts "あまり"
+num = num / 16
+puts num
+
+puts "最上位"
+three_num = num % 16
+puts three_num
+
+puts "あまり"
+num = num / 16
+puts num
+
+puts "#{num}" + "#{three_num}" + "#{two_num}" + "#{one_num}"
