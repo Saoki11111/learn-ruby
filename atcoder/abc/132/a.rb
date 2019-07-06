6d276e9187cc70cb6e6d21832588da98d39320c7
Shu_Aoki <cook.aoki@1pac.jp>
2019-07-06 12:35:24 +0900

    Add 処理コードを追加

--- atcoder/abc/132/a.rb
+++ atcoder/abc/132/a.rb
@@ -1,8 +1,4 @@
-# Sがちょうど 2種類の文字からなり
-# かつ現れる各文字はちょうど 2 回ずつ現れている
-# なら Yes を、そうでないなら No を出力せよ。
-
-# 長さ4の文字列Sの文字の種類数を判定
-# 文字の種類が何回現れているか判定
-# 各文字が2回か判定
-
+s = gets
+search = s.chomp.split(//).uniq
+search_count = search.count
+p search_result = (search_count == 2) ? 'Yes' : 'No'
