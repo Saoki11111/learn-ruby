a8ae34784d5580d7c0e526492155d769172bedcd
Shu_Aoki <cook.aoki@1pac.jp>
2019-07-06 15:34:10 +0900

    Update コンフリクト修正

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,7 +3,6 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < Minitest::Test
-<<<<<<< HEAD
   # テストメソッドが実行される前にこのメソッドが呼ばれる
   def setup
     @umeda = Gate.new(:umeda)
@@ -15,20 +14,6 @@ class GateTest < Minitest::Test
   # 150円で切符を購入する
   # 梅田で入場、十三で出場
   # 期待結果:出場できる
-=======
-  def test_gate
-    # 改札機オブジェクトの作成
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
-
-    # 150円の切符を購入して
-    ticket = Ticket.new(150)
-    # 梅田で乗車
-    umeda.enter(ticket)
-    # 十三で降車
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
->>>>>>> 4f6dfca5863bf8ef1020e4defac7af89f1e0630e
 
   def test_umeda_to_juso
     ticket = Ticket.new(150)
@@ -40,7 +25,7 @@ class GateTest < Minitest::Test
   # 150円で切符を購入する
   # 梅田で入場、三国で出場
   # 期待結果:出場できない
-
+  
   def test_umeda_to_mikuni_when_fare_is_not_enough
     ticket = Ticket.new(150)
     @umeda.enter(ticket)
